eb19a526940bbb720f73f5a9d52c03302cfe668e
Prashant Pandey <44068293+prashantpandey9@users.noreply.github.com>
2021-05-11 15:32:01 +0530

    Update server.js

--- src/Api/server.js
+++ src/Api/server.js
@@ -1,3 +1,4 @@
 // const server = 'http://127.0.0.1:8000';
-const server = 'http://13.235.100.207';
+// const server = 'http://13.235.100.207';
+const server = 'https://ayushbisht200121.pythonanywhere.com';
 export default server;
