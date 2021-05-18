162c9eff8b76f0d9f73d55b9fc4a8f62825a2064
Prashant Pandey <prashantpandey94551@gmail.com>
2021-05-18 10:36:41 +0530

    error resolved

--- src/Components/Statusform/Statusform.jsx
+++ src/Components/Statusform/Statusform.jsx
@@ -6,89 +6,90 @@ import axios from 'axios'
 import {useToasts} from 'react-toast-notifications'
 import cookie from 'react-cookies'
 export default function Statusform(props) {
-    const {addToast} = useToasts()
-    const initialState = {
-        id: props.id,
-        status: '',
-        migrated_to: '',
-        reason: ''
-    }
-    const [state, setState] = useState(initialState)
-    const handleSubmit = event => {
-        if (state.status==="Migrated"){
-            state.status="M"
+  const {addToast} = useToasts()
+  const initialState = {
+      id: props.id,
+      status: '',
+      migrated_to: '',
+      reason: ''
+  }
+  const [state, setState] = useState(initialState)
+  const handleSubmit = event => {
+      if (state.status==="Migrated"){
+          state.status="M"
+      }
+      else if (state.status==="Death"){
+        state.status="D"
+      }
+      else{
+          state.status="R"
+      }
+      event.preventDefault();
+      const eData = { 
+          patient_status: state.status
         }
-        else if (state.status==="Death"){
-          state.status="D"
-        }
-        else{
-            state.status="R"
-        }
-        event.preventDefault();
-        const eData = { 
-            patient_status: state.status
-          }
-        axios({
-            url: patientStatus+`${state.id}/`,
-            method: 'PATCH',
-            data: eData,
-            headers: {
-              Authorization: `Token ${cookie.load('token')}`,
-            },
-          })
-          .then(res=>{
-            if (res.data.status===200){
-              addToast(res.data.msg, { appearance: 'success' });
-              setState({ redirect: true});
-            }
-            else if (res.data.status===400){
-              setState({ redirect: false});
-              addToast("Error occurred try again!!", { appearance: 'error' });
-            }
-          })
-          .catch(error => {
-            setState({ redirect: false});
-            console.log(error)
-            addToast('The server is not excepting any request at this moment!! Try again later', { appearance: 'error' });
-          });
-          //  Facility change api
-          if (state.migrated_to.length || state.reason.length){
-            const eda = { 
-              migrated_to: state.migrated_to,
-              reason: state.reason,
-              patient_id: state.id
-            }
-            axios({
-              url: patientMigration,
-              method: 'POST',
-              data: eda,
-              headers: {
-                Authorization: `Token ${cookie.load('token')}`,
-              },
-            })
-            .then(res=>{
-              if (res.data.status===200){
-                addToast(res.data.msg, { appearance: 'success' });
-                setState({ redirect: true});
+      axios({
+          url: patientStatus+`${state.id}/`,
+          method: 'PATCH',
+          data: eData,
+          headers: {
+            Authorization: `Token ${cookie.load('token')}`,
+          },
+        })
+        .then(res=>{
+          if (res.data.status===200){
+            addToast(res.data.msg, { appearance: 'success' });
+            setState({ redirect: true});
+            if (state.migrated_to.length!==0 || state.reason.length!==0){
+              const eda = { 
+                migrated_to: state.migrated_to,
+                reason: state.reason,
+                patient_id: state.id
               }
-              else if (res.data.status===400){
+              axios({
+                url: patientMigration,
+                method: 'POST',
+                data: eda,
+                headers: {
+                  Authorization: `Token ${cookie.load('token')}`,
+                },
+              })
+              .then(res=>{
+                if (res.data.status===200){
+                  addToast(res.data.msg, { appearance: 'success' });
+                  setState({ redirect: true});
+                }
+                else if (res.data.status===400){
+                  setState({ redirect: false});
+                  addToast("Error occurred try again!!", { appearance: 'error' });
+                }
+              })
+              .catch(error => {
                 setState({ redirect: false});
-                addToast("Error occurred try again!!", { appearance: 'error' });
-              }
-            })
-            .catch(error => {
-              setState({ redirect: false});
-              console.log(error)
-              addToast('The server is not excepting any request at this moment!! Try again later', { appearance: 'error' });
-            });
+                console.log(error)
+                addToast('The server is not excepting any request at this moment!! Try again later', { appearance: 'error' });
+              });
+            }
           }
-          
-          
-      }
-      const handleChange = event =>{
-        setState({ ...state, [event.target.name]: event.target.value,         
+          else if (res.data.status===400){
+            setState({ redirect: false});
+            addToast("Error occurred try again!!", { appearance: 'error' });
+          }
+        })
+        .catch(error => {
+          setState({ redirect: false});
+          console.log(error)
+          addToast('The server is not excepting any request at this moment!! Try again later', { appearance: 'error' });
         });
-      }
+        //  Facility change api
+        
+        
+        
+    }
+    const handleChange = event =>{
+      setState({ ...state, [event.target.name]: event.target.value,         
+      });
+    }
 
     if (state.redirect){
         return <Redirect to='/list' />
