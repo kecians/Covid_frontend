be8e461291f751da3d4e469fd500e2330082ba22
Prashant Pandey <prashantpandey94551@gmail.com>
2021-05-18 10:43:15 +0530

    changed server

--- src/Api/server.js
+++ src/Api/server.js
@@ -1,3 +1,3 @@
 // const server = 'http://127.0.0.1:8000';
-const server  = 'https://ayushbisht20/0121.pythonanywhere.com';
+const server  = 'https://ayushbisht200121.pythonanywhere.com';
 export default server;
