9e5f32f32f775d1b5fa1fcb5df553ed405ef5a60
Prashant Pandey <prashantpandey94551@gmail.com>
2021-05-23 20:20:32 +0530

    added some changfes

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,10 +6,21 @@ export class Footer extends Component {
         return (
           <footer className="footer container-fluid ">
                   <div className="dropdown-divider"></div>
-                  <div className="row align-items-center justify-content-center p-1">
-                    <h6>Designed & Developed by</h6>
+                  <div className="row align-items-center justify-content-center ">
+                    <h6 className="border-bottom">
+                      Designed & Developed with <span className="fa fa-heart"></span> by:&nbsp;&nbsp;&nbsp;
+                      <a href="https://www.linkedin.com/in/ayush-bisht-9a5582192">Ayush Bisht  | &nbsp; </a> 
+                      <a href="https://prashantpandey.ml/">  Prashant Pandey | &nbsp;</a>  
+                      <a href="https://www.linkedin.com/in/shubham-tripathi-727424144"> Shubham Tripathi</a>
+                    </h6>
+                    
                   </div>
-                  
+                 
+                  <div className="row align-items-center justify-content-center">
+                    <h6 className="border-bottom">Guided by: &nbsp;&nbsp;&nbsp;<a href="/">Dr. Kunwar Singh Vaisla (H.O.D. CSE BTKIT ALMORA) &nbsp; </a></h6>
+                     
+                  </div>
+
                   <div className="row align-items-center justify-content-center p-1">
                     <h6>&copy;Copyright ©2021 All rights reserved</h6>
                   </div>
