a7f783ffd3a92363fccd7a4983365c2cb5661cb4
ayushbisht2001 <bisht.sanu2001@gmail.com>
2021-05-23 09:28:27 -0700

    styled footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,26 +4,25 @@ import './Footer.scss'
 export class Footer extends Component {
     render() {
         return (
-          <footer className="footer container-fluid ">
-                  <div className="dropdown-divider"></div>
-                  <div className="row align-items-center justify-content-center ">
-                    <h6 className="border-bottom">
-                      Designed & Developed with <span className="fa fa-heart"></span> by:&nbsp;&nbsp;&nbsp;
-                      <a href="https://www.linkedin.com/in/ayush-bisht-9a5582192">Ayush Bisht  | &nbsp; </a> 
-                      <a href="https://prashantpandey.ml/">  Prashant Pandey | &nbsp;</a>  
-                      <a href="https://www.linkedin.com/in/shubham-tripathi-727424144"> Shubham Tripathi</a>
-                    </h6>
-                    
-                  </div>
+          <footer className="footer container-fluid p-0 m-0  ">
+                  <div className="dropdown-divider"></div>                  
+                    <p className=" text-center">
+                      Designed  &amp;  Developed with <span className="fa fa-heart" style = {{color : "red"}}> </span> by:&ensp; 
+                      <a href="https://www.linkedin.com/in/ayush-bisht-9a5582192">Ayush&nbsp;Bisht&nbsp;|&nbsp; </a> 
+                      <a href="https://prashantpandey.ml/">&nbsp;Prashant&nbsp;Pandey&nbsp;|&nbsp;</a>  
+                      <a href="https://www.linkedin.com/in/shubham-tripathi-727424144">&nbsp;Shubham&nbsp;Tripathi</a>
+                    </p>
+                    <div className="divider-p"></div>
+                  
                  
-                  <div className="row align-items-center justify-content-center">
-                    <h6 className="border-bottom">Guided by: &nbsp;&nbsp;&nbsp;<a href="/">Dr. Kunwar Singh Vaisla (H.O.D. CSE BTKIT ALMORA) &nbsp; </a></h6>
-                     
-                  </div>
+                 
+                    <p className=" text-center">Guided by : &ensp;<a href="/">Dr. Kunwar Singh Vaisla (H.O.D. CSE BTKIT ALMORA) &ensp; </a></p>
+                    <div className="divider-p"></div>
+               
 
-                  <div className="row align-items-center justify-content-center p-1">
-                    <h6>&copy;Copyright ©2021 All rights reserved</h6>
-                  </div>
+              
+                    <p className=" text-center ">&copy;Copyright ©2021 All rights reserved</p>
+                 
           </footer>
         )
     }
