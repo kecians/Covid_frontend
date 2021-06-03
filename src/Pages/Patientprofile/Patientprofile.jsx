37fa00f2bba06c044fc3a11d70057f69835da39a
Prashant Pandey <prashantpandey94551@gmail.com>
2021-06-03 12:10:39 +0530

    fixx loader in pprofile

--- src/Pages/Patientprofile/Patientprofile.jsx
+++ src/Pages/Patientprofile/Patientprofile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Pprofile from '../../Components/Pprofile/Pprofile'
 export default function Patientprofile(props) {
     return (
-        <div className="container-fluid bg-silver pt-4">
+        <div className="container-fluid bg-silver p-0">
             <Pprofile id = {props.match.params.id} contact = {props.match.params.contact} />
         </div>
     )
